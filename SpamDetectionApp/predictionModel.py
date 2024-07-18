d8a29d809562e32bff4eb21181a21c90374d616c
felekekinfe <felekekinfe@gmail.com>
2024-07-18 08:15:11 -0400

    commit

--- SpamDetectionApp/predictionModel.py
+++ SpamDetectionApp/predictionModel.py
@@ -1,6 +1,7 @@
 
 import numpy as np
-import pandas as pd
+import pandas as pdo
+
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
